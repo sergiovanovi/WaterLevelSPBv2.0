b4782f4cf3b233e6c7a8fe1df8a23372e8bf4c1b
sergiovanovi <sergiovanovi@mail.ru>
2017-03-09 17:10:26 +0300

    add requestMapping("/users") in MainController

--- src/main/java/com/sergiovanovi/web/MainController.java
+++ src/main/java/com/sergiovanovi/web/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -21,13 +22,13 @@ public class MainController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Model model) {
         LOG.info("send to profile");
         return "redirect:/profile";
     }
 
-    @RequestMapping(value = "/profile")
+    @RequestMapping(value = "/profile", method = RequestMethod.GET)
     public String metersList(Model model) {
         model.addAttribute("meters", meterService.getAll());
         model.addAttribute("lastMeter", meterService.getLast());
@@ -36,12 +37,12 @@ public class MainController {
         return "profile";
     }
 
-    @RequestMapping(value = "/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginPage(Model model) {
         return "login";
     }
 
-    @RequestMapping(value = {"/users"})
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
     public String usersList(Model model) {
         model.addAttribute("users", userService.getAll());
         LOG.info("send to users");
