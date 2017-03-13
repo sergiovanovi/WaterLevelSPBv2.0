7cc44ab542c66eacf8b3beb319b36b0205232cc2
sergiovanovi <sergiovanovi@mail.ru>
2017-03-13 17:05:33 +0300

    util parsMeter made it easier

--- src/main/java/com/sergiovanovi/util/MeterParserAndMailSender.java
+++ src/main/java/com/sergiovanovi/util/MeterParserAndMailSender.java
@@ -37,39 +37,40 @@ public class MeterParserAndMailSender {
         LineNumberReader reader = null;
         try {
             URL url = new URL("http://www.pasp.ru/op-info-weather?mode=current");
-            try {
-                reader = new LineNumberReader(new InputStreamReader(url.openStream()));
-                String stringMeter = reader.readLine();
+            reader = new LineNumberReader(new InputStreamReader(url.openStream()));
+            String stringMeter;
 
-                //TODO Make it easier and ask the measurement provider for a direct link
-                while (stringMeter != null) {
-                    if (reader.getLineNumber() == 184) {
-                        stringMeter = stringMeter.trim();
-                        stringMeter = stringMeter.substring(stringMeter.indexOf(":") + 2, stringMeter.lastIndexOf(" "));
-                        double waterLevel = Double.parseDouble(stringMeter);
-                        Meter meter = new Meter();
-                        meter.setDateTime(LocalDateTime.now());
-                        meter.setLevel(waterLevel);
-                        meterService.save(meter);
-                        checkMeter(waterLevel);
-                        LOG.info(LocalDateTime.now() + " Measurement is read and verified");
-                        break;
-                    }
-                    stringMeter = reader.readLine();
+            //TODO Make it easier and ask the measurement provider for a direct link
+            do {
+                stringMeter = reader.readLine();
+                if (reader.getLineNumber() == 184) {
+                    stringMeter = stringMeter.trim();
+                    stringMeter = stringMeter.substring(stringMeter.indexOf(":") + 2, stringMeter.lastIndexOf(" "));
+                    double waterLevel = Double.parseDouble(stringMeter);
+                    Meter meter = new Meter();
+                    meter.setDateTime(LocalDateTime.now());
+                    meter.setLevel(waterLevel);
+                    meterService.save(meter);
+                    checkMeter(waterLevel);
+                    LOG.info(LocalDateTime.now() + " Measurement is read and verified");
+                    break;
                 }
-            } catch (IOException e) {
-                LOG.error(LocalDateTime.now() + " Can not open a stream");
-            } finally {
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (IOException e) {
-                        LOG.error(LocalDateTime.now() + " Can not close a stream");
-                    }
-                }
-            }
+            } while (stringMeter != null);
+
         } catch (MalformedURLException e) {
             LOG.error(LocalDateTime.now() + " Web-site does not respond");
+
+        } catch (IOException e) {
+            LOG.error(LocalDateTime.now() + " Can not open a stream");
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    LOG.error(LocalDateTime.now() + " Can not close a stream");
+                }
+            }
         }
     }
 
