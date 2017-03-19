23cf8722d7afe39238bda488bda9c1e9529ad32c
machete <inbox@sergiovanovi.com>
2017-03-19 21:23:38 +0300

    fix mock

--- src/test/mock/MockUserService.java
+++ src/test/mock/MockUserService.java
@@ -1,14 +1,16 @@
 package mock;
 
 import com.sergiovanovi.model.User;
+import com.sergiovanovi.model.enums.Role;
 import com.sergiovanovi.util.PasswordUtil;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MockUserService {
-    public static User user = new User("sergiovanovi@mail.ru", PasswordUtil.encode("123123"), -5, 5);
-    public static User admin = new User("inbox@sergiovanovi.com", PasswordUtil.encode("123456"), -10, 10);
-    public static User expected = new User("expected@gmail.com", "321321", -12, 12);
+    public static User user = new User("sergiovanovi@mail.ru", PasswordUtil.encode("123123"), -5, 5, Collections.singleton(Role.ROLE_USER));
+    public static User admin = new User("inbox@sergiovanovi.com", PasswordUtil.encode("123456"), -10, 10, Collections.singleton(Role.ROLE_ADMIN));
+    public static User expected = new User("expected@gmail.com", "321321", -12, 12, Collections.singleton(Role.ROLE_USER));
     public static List<User> users = Arrays.asList(user, admin);
 }
